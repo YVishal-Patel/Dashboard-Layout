44cf4faf74d7003c383f2ca084512a4467945106
Vishal Patel <yvishalpatelz3245@gmail.com>
2022-09-27 13:03:35 +0530

    updated

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -17,7 +17,7 @@ export const Navbar = () => {
         </div>
         <div className="header_icon">
         <svg xmlns="http://www.w3.org/2000/svg" className='left-right-icon' enable-background="new 0 0 24 24" height="20px" viewBox="0 0 24 24" width="15px" fill="#000000"><rect fill="none" height="34" width="34"/><path d="M15,5l-1.41,1.41L18.17,11H2V13h16.17l-4.59,4.59L15,19l7-7L15,5z"/></svg>
-        </div>git
+        </div>
 
         <div className="navbar_user_heading">
           <span className='navbar_heading_Data'>User Stories</span>
